e3e97e5716aed6ddf65c752f2790d588d1d20174
Pascal P <pascal.phelipot@gmail.com>
2017-05-22 16:15:41 -0400

    Fix inherit

--- app/controllers/utils_controller.rb
+++ app/controllers/utils_controller.rb
@@ -1,6 +1,6 @@
 require_relative '../../lib/application_controller'
 
-class UtilsController < Sinatra::Base
+class UtilsController < ApplicationController
   get '/sass/*.css' do
     content_type 'text/css', :charset => 'utf-8'
     filename= params[:splat].first
